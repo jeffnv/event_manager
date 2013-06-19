fb2a42baceabd6bbf259e5e2c3793b7c471e9911
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-19 06:14:49 -0700

    just after moving clean zipcodes to a method

--- event_manager.rb
+++ event_manager.rb
@@ -1,15 +1,31 @@
+require "csv"
+
 puts "Event manager initialized!"
 
+def clean_zipcode(zip)
+  zip ||=""
+  
+  result = zip.split('')
+  
+  while result.count != 5 do
+    if(result.count < 5)
+      result.insert(0, '0')
+    elsif(result.count > 5)
+      result = result[0..4]
+    end
+  end
+  result.join('')
 
+end
 FILE_TO_READ = "event_attendees.csv"
 if(File.exist? (FILE_TO_READ))
   
-  lines = File.readlines FILE_TO_READ
-
-  lines.each_with_index do |line, index|
-    next if (index == 0)
-    columns = line.split(",")
-    puts columns[2]
+  rows = (CSV.open FILE_TO_READ, headers: true, header_converters: :symbol)
+  
+  rows.each do |row|
+    name = row[:first_name]
+    zip = clean_zipcode(row[:zipcode])
+    puts "name:#{name} | zip:#{zip}"
   end
   
 end
