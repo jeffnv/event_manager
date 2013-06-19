e5171a4a2bb34f9e1d64cc26a093b8ba685119a3
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-18 17:57:01 -0700

    finished section zero of event manager

--- event_manager.rb
+++ event_manager.rb
@@ -1,1 +1,15 @@
 puts "Event manager initialized!"
+
+
+FILE_TO_READ = "event_attendees.csv"
+if(File.exist? (FILE_TO_READ))
+  
+  lines = File.readlines FILE_TO_READ
+
+  lines.each_with_index do |line, index|
+    next if (index == 0)
+    columns = line.split(",")
+    puts columns[2]
+  end
+  
+end
