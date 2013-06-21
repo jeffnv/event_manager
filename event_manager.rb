8c3259780b2288ddbc5aa36b17d10073af3afae2
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-21 06:01:14 -0700

    ok, followup generates a list of names and numbers

--- event_manager.rb
+++ event_manager.rb
@@ -5,6 +5,7 @@ require 'erb'
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 EVENT_ATTENDEES = "event_attendees.csv"
 FORM_LETTER = "form_letter.erb"
+FOLLOWUP_FILENAME = "followup.txt"
 puts "Event manager initialized!"
 
 
@@ -18,7 +19,12 @@ def clean_phone(phone)
   when 10
     result = num.insert(0, '1')
   end
-  # puts result
+  
+  unless result.empty?
+    result.insert(1,'-')
+    result.insert(5, '-')
+    result.insert(9, '-')
+  end
   result
 end
 
@@ -44,8 +50,19 @@ def save_form_letter(content, id)
   end
 end
 
-letter_template = load_form_letter
+def save_followup_list(content)
+  Dir.mkdir("output") unless Dir.exists? "output"
+  
+  filename = "output//#{FOLLOWUP_FILENAME}"
+  
+  File.open(filename, 'w') do |file|
+    file.puts content
+  end
+end
 
+letter_template = load_form_letter
+names = []
+numbers = []
 if(File.exist? (EVENT_ATTENDEES))
   
   rows = (CSV.open EVENT_ATTENDEES, headers: true, header_converters: :symbol)
@@ -53,14 +70,19 @@ if(File.exist? (EVENT_ATTENDEES))
   rows.each do |row|
     template = ERB.new letter_template
     id = row[0]
-    name = row[:first_name]
-    number = clean_phone(row[:homephone])
+    names << name = row[:first_name]
+    numbers << number = clean_phone(row[:homephone])
     zip = clean_zipcode(row[:zipcode])
     legislators = legislators_by_zip(zip)
     letter = template.result(binding)
     save_form_letter(letter, id)
    
   end
+  follow_up_content = ""
+  names.each_with_index do |name, index|
+    follow_up_content << "#{name} #{numbers[index]}\n"
+  end
+  save_followup_list follow_up_content
   
 end
 
